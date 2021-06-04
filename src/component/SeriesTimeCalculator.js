b1858d93e1fce07fd83ac60ac4d87d358cd98bbc
dhruvpeter <dhruveldhopeter@gmail.com>
2021-06-04 16:14:30 +0530

    Added basic page structure

--- src/component/SeriesTimeCalculator.js
+++ src/component/SeriesTimeCalculator.js
@@ -1,9 +1,66 @@
-import React from 'react'
+import React, { Component } from "react";
+import "./SeriesTimeCalculator.css";
+export default class SeriesTimeCalculator extends Component {
+  state = {
+    input: "",
+    items: [],
+  };
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+  storeItems = (event) => {
+    event.preventDefault();
+    const { input } = this.state;
+    // const allItems = this.state.items;
+    // allItems.push(input);
+    // this.setState({
+    //   items: allItems,
+    // });
+    this.setState({
+      items: [...this.state.items, input],
+      input: "",
+    });
+  };
+  deleteItem = (key) => {
+    //   const allItems=this.state.items;
+    //   allItems.splice(key,1);
+    //   this.setState({
+    //       items:allItems
+    //   })
+    this.setState({
+      items: this.state.items.filter((data, index) => index !== key),
+    });
+  };
 
-export default function SeriesTimeCalculator() {
+  render() {
+    const { input, items } = this.state;
     return (
-        <div>
-            Series Time Calculator
-        </div>
-    )
+      <div className="input-container">
+        <form className="input-section" onSubmit={this.storeItems}>
+          <h1>Series Time Calculator</h1>
+          <input
+            type="text"
+            name="input"
+            onChange={this.handleChange}
+            value={input}
+            placeholder="Type in a TV Show"
+          />
+        </form>
+        <ul>
+          {items.map((data, index) => (
+            <li key={index}>
+              {data}
+
+              <i
+                className="fas fa-times"
+                onClick={() => this.deleteItem(index)}
+              ></i>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
