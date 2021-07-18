1e21bfc61ca9daa82170a9018c2b52d8b6a348de
psdhanesh7 <psdhanesh2000@gmail.com>
2021-07-18 19:24:16 +0530

    add api call

--- src/component/SeriesTimeCalculator.js
+++ src/component/SeriesTimeCalculator.js
@@ -1,66 +1,76 @@
-import React, { Component } from "react";
+import { useState } from "react";
 import "./SeriesTimeCalculator.css";
-export default class SeriesTimeCalculator extends Component {
-  state = {
-    input: "",
-    items: [],
+
+import axios from "axios";
+
+export default function SeriesTimeCalculator() {
+  const [input, setInput] = useState("");
+  const [results, setResults] = useState([]);
+
+  const handleChange = (event) => {
+      setInput(event.target.value);
   };
-  handleChange = (event) => {
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
-  };
-  storeItems = (event) => {
+
+  const storeItems = (event) => {
     event.preventDefault();
-    const { input } = this.state;
-    // const allItems = this.state.items;
-    // allItems.push(input);
-    // this.setState({
-    //   items: allItems,
-    // });
-    this.setState({
-      items: [...this.state.items, input],
-      input: "",
-    });
-  };
-  deleteItem = (key) => {
-    //   const allItems=this.state.items;
-    //   allItems.splice(key,1);
-    //   this.setState({
-    //       items:allItems
-    //   })
-    this.setState({
-      items: this.state.items.filter((data, index) => index !== key),
-    });
+
+    async function updateResult() {
+      const title = input;
+
+      const options = {
+        method: 'GET',
+        url: 'https://imdb8.p.rapidapi.com/title/find',
+        params: {q: input},
+        headers: {
+          'x-rapidapi-key': '350b05e5efmshbe4a30ce50ae94ap1bc047jsnd95e5bf5097e',
+          'x-rapidapi-host': 'imdb8.p.rapidapi.com'
+        }
+      };
+
+      try {
+        const response = await axios.request(options);
+        const result = response.data.results.filter((result) => result.titleType === 'tvSeries' && result.title.toLowerCase() === title.toLowerCase());
+        console.log(result);
+
+        setResults([...results, result[0]])
+      } catch(err) {
+        console.log(err.message);
+      }
+
+    }
+    updateResult();
   };
 
-  render() {
-    const { input, items } = this.state;
-    return (
-      <div className="input-container">
-        <form className="input-section" onSubmit={this.storeItems}>
-          <h1>Series Time Calculator</h1>
-          <input
-            type="text"
-            name="input"
-            onChange={this.handleChange}
-            value={input}
-            placeholder="Type in a TV Show"
-          />
-        </form>
-        <ul>
-          {items.map((data, index) => (
-            <li key={index}>
-              {data}
-
-              <i
-                className="fas fa-times"
-                onClick={() => this.deleteItem(index)}
-              ></i>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
+  const deleteItem = (key) => {
+    setResults(results.filter((data, index) => index !== key));
   }
+
+
+  return (
+    <div className="input-container">
+      <form className="input-section" onSubmit={storeItems}>
+        <h1>Series Time Calculator</h1>
+        <input
+          type="text"
+          name="input"
+          onChange={handleChange}
+          value={input}
+          placeholder="Type in a TV Show"
+        />
+      </form>
+      <ul>
+        {results.map((data, index) => (
+          <li key={index}>
+            {data.title}
+
+            <i
+              className="fas fa-times"
+              onClick={() => deleteItem(index)}
+            ></i>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+
 }
